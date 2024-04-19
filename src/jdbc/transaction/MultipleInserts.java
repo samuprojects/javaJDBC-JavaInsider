83495fc86d34f17a0614de3057e1fd911e18f06d
Samuel <samugit.projects@gmail.com>
2024-04-19 12:25:18 -0300

    Aula17 Analisando o problema da falta da transação

--- src/jdbc/transaction/MultipleInserts.java
+++ src/jdbc/transaction/MultipleInserts.java
@@ -1,26 +1,25 @@
 package jdbc.transaction;
 
 /*
-* Realizando inserts múltiplos sem transação e seus efeitos colaterais
-* Para isso inserido um registro de pessoa e um telefone associado a ela.
-* Para recuperar o id gerado utilizamos o getGeneratedKeys() e depois o
-* getInt porque a informação da chave gerada está dentro do ResultSet já no
-* primeiro registro, primeira coluna.
-* Para isso acrescentado também essa instrução no SQL utilizando
-* Statement.RETURN_GENERATED_KEYS, pois havia dado erro sem isso.
+* Gerado um erro de foreign key, ao tentar inserir um registro referenciando outro
+* da tabela customer que não existe
 *
-* Depois foi confirmado a inserção graficamente no workbench e excluído os registros da "maria".
-* (observado que o professor estava com a estrutura do banco diferente das aulas anteriores (não tinha a coluna city)).
+* Como as operações são atômicas e separadas por si só, o primeiro insert foi realizado e o segundo deu erro.
 *
-* Depois seguiu para inserir os dados na tabela phone utilizando o id gerado na primeira tabela (no primeiro insert)
+* Com isso no workbench a tabela de telefone não recebeu o telefone para a outra "maria" inserida.
 *
-* Apesar de tudo funcionar não é o cenário ideal conforme o professor vai mostrar mais a frente
+* Considerando um cenário em que os usuários inseridos precisam, obrigatoriamente, de um telefone associado, a
+* aplicação tornou-se inconsistente.
+*
+* Por isso a importância de executá-las em uma única transação.
+*
 * */
 
 import java.sql.*;
 
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
+@SuppressWarnings("ALL")
 public class MultipleInserts {
 
     public static void main(String[] args) throws  Exception {
@@ -37,12 +36,12 @@ public class MultipleInserts {
 
                 try (ResultSet rs = stmt.getGeneratedKeys()){
                     rs.next();
-                    customerId = rs.getInt(1); // vai receber o valor de customerId
+                    customerId = rs.getInt(1);
                 }
             }
 
             try (PreparedStatement stmt = conn.prepareStatement("INSERT INTO phone (customer_id, description, number) VALUES (?, ?, ?)")) {
-                stmt.setInt(1, customerId);
+                stmt.setInt(1, 9999); // erro causado para testar consequencia do insert sem transação, valor original customerId no lugar de 9999
                 stmt.setString(2, "Celular");
                 stmt.setString(3, "(21) 3498-49389");
                 stmt.executeUpdate();
